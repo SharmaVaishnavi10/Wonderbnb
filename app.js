ffce7d26533ddbe9e831d2f99fd0dc84abd0637a
SharmaVaishnavi10 <vaishnaviss2005@gmail.com>
2026-07-05 00:04:52 +0530

    Add Listing model and update app

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wornderbnb"
 
-main().then(() => {
+main()
+  .then(() => {
     console.log("Connected to MongoDB");
-}).catch((err) => {
+})
+  .catch((err) => {
     console.error("Error connecting to MongoDB:", err);
 });
 
 async function main() {
     await mongoose.connect(MONGO_URL);
 }
+
 app.get("/" ,(req,res) => {
     res.send("Hlo i am root")
-})
+});
+
+app.get("/testListing", async (req, res) => {
+    let sampleListing = new Listing({
+        title: "My New Villa",
+        description: "A beautiful villa with stunning views.",
+        price: 1200,
+        location: "Calangute, Goa",
+        country: "India",
+    })
+
+    await sampleListing.save();
+    res.send("Sample listing created!");
+    console.log("Sample listing created and saved to the database.");
+
+});
+
 
 app.listen(8080,() => {
     console.log("connected to server");
-})
+});
---
+++ models/listing.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type:String,
+        required: true,
+    },
+    description: String,
+    image: {
+        type: String,
+        default: "https://unsplash.com/photos/orange-robot-character-with-a-pink-flower-xFmbzYtabOY",
+        set: (v) => v === "" ? "https://unsplash.com/photos/orange-robot-character-with-a-pink-flower-xFmbzYtabOY": v,
+    },
+    price:Number,
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
